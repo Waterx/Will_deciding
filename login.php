abd04e9ab2fc575289866fe7f1cf7768d88a8273
waterx <hzx915184874@gmail.com>
2017-05-29 23:25:24 +0800

    该开始做manage.php了

--- login.php
+++ login.php
@@ -9,7 +9,15 @@ if($is->fetch_array()){
     $row=$is->fetch_array();
     //var_dump($is->fetch_array());
     $_SESSION['account']=$account;
-    header("Location:will_deciding.php");
+    $patten = '/S/';
+    $isStu = preg_match($patten,$account);
+    var_dump($isStu);
+    if($isStu){
+        header("Location:will_deciding.php");
+    }
+    else{
+        header("Location:will_manage.php");
+    }
     //var_dump($_SESSION);
     echo"登陆成功";
 }
--- will_submit.php
+++ will_submit.php
@@ -3,10 +3,25 @@ session_start();
 var_dump($_POST['will1']);
 var_dump($_SESSION);
 include("db.php");
+//以下为查找该账号是否提交过
+$sql_search = "SELECT * FROM will_submit WHERE id='{$_SESSION['account']}'";
+//var_dump($mysqli->query($sql_search)->fetch_row());
+//var_dump($mysqli->query($sql_search)->fetch_array());
 
+if($mysqli->query($sql_search)->fetch_row())
+{
+    echo "您已填报过<br>";
+}
+//以下为插入表
 $sql_insert = "INSERT INTO will_submit (`id`, `will1`, `will2`, `will3`) 
                VALUES ('{$_SESSION['account']}',
                '{$_POST['will1']}', '{$_POST['will2']}', '{$_POST['will3']}')";
 
-
+$info = $mysqli->query($sql_insert);
+if( $info == true ){
+    echo"填报成功";
+}
+else{
+    echo"填报失败";
+}
 ?>
