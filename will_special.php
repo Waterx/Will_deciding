165cbb7be7dcc54a8963303317ca6c056724314b
waterx <hzx915184874@gmail.com>
2017-06-04 14:37:29 +0800

    分配特殊申请的功能完成

--- will_special.php
+++ will_special.php
@@ -22,32 +22,14 @@
     echo"<form action='will_specialmanage.php' method='POST'>";
     $count = 1;
     while($row = mysqli_fetch_assoc($info_major)){
-     
-        echo "<input type='radio' name='will' value=$count> $row[major]";
+        //为了传id，performance，major_id只能把他们连成字符串
+        $mix=$id.'*'.$performance.'*'.$count;
+        //var_dump($mix);
+        echo "<input type='radio' name='will' value=$mix> $row[major]";
         $count++;
     }
     echo"<br><input type='submit' value='提交' class='button'>";
     echo"</form>";
-    //以下的查询是查找给该学生添加的专业是否还有名额
-    $majorid = (int)$_POST['will'];
-    $sql_search = "SELECT * FROM major WHERE major_id=$majorid";
-    $row_ = $mysqli->query($sql_search)->fetch_assoc();
-    $majornum = $row_['num_of_stu'];
-    if($majornum!=0){
-        //以下的查询是在enroll表中插入特殊申请学生的成绩和录取专业
-        $sql_search = "UPDATE enroll SET performance=$performance,major_id=$majorid
-                      WHERE id='{$id}'";
-        $is = $mysqli->query($sql_search);
-        //以下的查询是来更新major表中剩余名额
-        if($is){
-            $sql_update_num = "UPDATE major SET num_of_stu = num_of_stu-1 WHERE major_id = $majorid;";
-            $is = $mysqli->query($sql_update_num);
-            echo"分配成功";
-        }
-    }
-    else if ($majornum==0){
-        echo"该专业已满";
-    }
 
 
 ?>
--- will_specialmanage.php
+++ will_specialmanage.php
@@ -1,16 +1,31 @@
 <?php
 session_start();
 include("db.php");
-var_dump($_POST);
-$majorid = (int)$_POST['will'];
-var_dump($majorid);
-//以下的查询是查找给该学生添加的专业是否还有名额
+//var_dump($_POST);
+
+//把传过来的字符串分解
+$content = explode('*',$_POST['will']);
+//var_dump($content);
+$majorid = $content[2];
+$performance = $content[1];
+$id = $content[0];
+//以下的查询是查找给该学生添加的专业是否还有名额 
 $sql_search = "SELECT * FROM major WHERE major_id=$majorid";
 $row_ = $mysqli->query($sql_search)->fetch_assoc();
-if($row_['num_of_stu']!=0){
-    //$sql_add = 
+$majornum = $row_['num_of_stu'];
+if($majornum!=0){
+    //以下的查询是在enroll表中插入特殊申请学生的成绩和录取专业
+    $sql_search = "UPDATE enroll SET performance=$performance,major_id=$majorid
+                  WHERE id='{$id}'";
+    $is = $mysqli->query($sql_search);
+    //以下的查询是来更新major表中剩余名额
+    if($is){
+        $sql_update_num = "UPDATE major SET num_of_stu = num_of_stu-1 WHERE major_id = $majorid;";
+        $is = $mysqli->query($sql_update_num);
+        echo"分配成功";
+    }
 }
-else{
+else if ($majornum==0){
     echo"该专业已满";
 }
 
